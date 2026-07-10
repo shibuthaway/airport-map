102f578c89a2c34854480fd4481a43df10e45bc9
Dheeraj Mishra <dheeraj@solarman.in>
2026-07-10 20:56:23 +0530

    Revert: Remove DXF Studio functionality

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import DxfStudio from './components/DxfStudio/DxfStudio.jsx'
-
-const isDxfMode = window.location.pathname === '/dxf';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {isDxfMode ? <DxfStudio /> : <App />}
+    <App />
   </StrictMode>,
 )
